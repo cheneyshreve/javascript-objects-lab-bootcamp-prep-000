79fa46bc17b4a738110236908ca0fbbf153f2d75
Cheney Shreve <cheney.shreve@gmail.com>
2017-09-25 23:33:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  var object[key] = value
+  var object['key'] = value
   return object
 }
 

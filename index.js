1ca00ad7a87942ed63a48e765328d0d213dc9a93
Cheney Shreve <cheney.shreve@gmail.com>
2017-09-25 23:19:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ function recipesObject() {
 
 
 function updateObjectWithKeyAndValue(obj, key, value){
-  Object.assign({}, obj, {key: value})
+  Object.assign({}, obj, { [key]: value})
+  return obj
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){

eef06b3219cf5259e76c2a5e2fb96f46f3139fa3
Cheney Shreve <cheney.shreve@gmail.com>
2017-09-25 23:11:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function objects() {
+  var recipes = new Object();
+  return recipes
+}
+
+
+function updateObjectWithKeyAndValue(object, key, value){
+
+
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+
+}
+
+function deleteFromObjectByKey(object, key){
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+
+}
